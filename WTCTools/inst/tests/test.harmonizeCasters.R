ae613f61ce6337d716e03e0efd31bf141f7dc170
Chris Campbell <ccampbell@mango-solutions.com>
2017-09-16 01:14:37 +0100

    added new casters and regex fix in Saeryn2

--- WTCTools/inst/tests/test.harmonizeCasters.R
+++ WTCTools/inst/tests/test.harmonizeCasters.R
@@ -4,4 +4,10 @@ context("check harmonizeCasters")
 test_that("harmonizeCasters", {
     expect_equal(suppressWarnings(harmonizeCasters(c("Vlad", "Vlad1", "Vald 1"))), 
         expected = c("Vladimir 1", "Vladimir 1", "Vald 1"))
+    
+    expect_equal(harmonizeCasters("Saeryn 2 & Rhyas 2"), 
+        expected = "Saeryn 2 & Rhyas 2")
+    
+    expect_equal(harmonizeCasters(c("Saeryn 2", "Saeryn 2 & Rhyas 2")), 
+        expected = c("Saeryn 2 & Rhyas 2", "Saeryn 2 & Rhyas 2"))
 })
