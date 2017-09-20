03f857dd205e1d0d456943751052c41826edb0ac
Chris Campbell <ccampbell@mango-solutions.com>
2017-09-20 02:09:53 +0100

    text cleaning tools

--- WTCTools/R/utils.R
+++ WTCTools/R/utils.R
@@ -22,3 +22,54 @@ ind <- function(n) {
     return(out)
 }
 
+#' @title Convert Accented Characters to Plain
+#' @description uses \code{chartr} to transpose characters.
+#' https://stackoverflow.com/questions/17517319/r-replacing-foreign-characters-in-a-string
+#' @param s character vector
+#' return character vector
+#' @export
+#' @examples
+#' toPlain("Štepan Slavik")
+
+toPlain <- function(s) {
+    # 1 character substitutions
+    old1 <- "ÂÅÃŠšžþàáâãäåçèéêëìíîïðñòóôõöùúûüý"
+    new1 <- "AAASszyaaaaaaceeeeiiiidnooooouuuuy"
+    s1 <- chartr(old1, new1, s)
+
+    # 2 character substitutions
+    old2 <- c("œ", "ß", "æ", "ø")
+    new2 <- c("oe", "ss", "ae", "oe")
+    s2 <- s1
+    for (i in seq_along(old2)) {
+        s2 <- gsub(pattern = old2[i], 
+            replacement = new2[i], x = s2, fixed = TRUE)
+    }
+    s2
+}
+
+#' @title Print names that are similar
+#' @examples
+#' showSimilarNames(c("Minnie", "Millie", "Ernestine"))
+
+showSimilarNames <- function(s, dist = 4L) {
+    pn <- stri_trans_general(str = s, id = "latin-ascii")
+    
+    for (let in letters) {
+        pnlet <- grep(pattern = paste0("^", let), 
+            x = casefold(pn), value = TRUE)
+        if (length(pnlet) > 0) {
+            dmatlet <- adist(x = pnlet)
+            dmatlet <- dmatlet * lower.tri(dmatlet)
+            pnlet <- pn[casefold(pn) %in% pnlet]
+            for (rowlet in seq_len(nrow(dmatlet))) {
+                x <- dmatlet[rowlet, ]
+                if (any(x[x != 0] < dist)) {
+                    for (hits in pnlet[x < dist & x > 0]) {
+                        message(pnlet[rowlet], "  :  ", hits)
+                    }
+                }
+            }
+        }
+    }
+}
